12970725514234f52965f7ad3cbf33413ac81cf7
janelin612 <wgl1242254@gmail.com>
2014-05-06 20:06:37 +0800

    Integer 改為 static 形式

--- d0310/src/d0310/convert65.java
+++ d0310/src/d0310/convert65.java
@@ -5,10 +5,11 @@ public class convert65 {
 	static int a=65;
 	public static void main(String[] args){
 		System.out.println(a+":");
-		System.out.println(new Integer(a).toBinaryString(a)); //二進位
-		System.out.println(new Integer(a).toOctalString(a));  //八進位
-		System.out.println("0x"+new Integer(a).toHexString(a));  //十六進位
-		System.out.println("\'\\u"+new Integer(a).toHexString(a)+"\'"); //Unicode
+
+		System.out.println(Integer.toBinaryString(a)); //二進位
+		System.out.println(Integer.toOctalString(a));  //八進位
+		System.out.println("0x"+Integer.toHexString(a));  //十六進位
+		System.out.println("\'\\u"+Integer.toHexString(a)+"\'"); //Unicode
 	}
 
 }
