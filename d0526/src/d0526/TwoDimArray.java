f3f8fe6aef8a7aeb391ca7da0b47f7ab27d678ad
janelin612 <wgl1242254@gmail.com>
2014-05-26 19:53:05 +0800

    內部改為一維陣列形式

--- d0526/src/d0526/TwoDimArray.java
+++ d0526/src/d0526/TwoDimArray.java
@@ -1,30 +1,21 @@
 package d0526;
 //自定義的陣列資料結構
 public class TwoDimArray {
-	private int LU,RU,LD,RD;
+	private int[] element;
 	
 	public TwoDimArray(int leftUp,int rightUp,int leftDown,int rightDown){
-		LU=leftUp;
-		RU=rightUp;
-		LD=leftDown;
-		RD=rightDown;
+		element=new int[4];
+		element[0]=leftUp;
+		element[1]=rightUp;
+		element[2]=leftDown;
+		element[3]=rightDown;
 	}
 	
 	public int getElement(int i){
-		switch(i){
-		case 0:
-			return LU;
-		case 1:
-			return RU;
-		case 2:
-			return LD;
-		case 3:
-			return RD;
-		}
-		return 0;
+		return element[i];
 	}
 	
 	public void print(){
-		System.out.println(LU+" "+RU+"\n"+LD+" "+RD);
+		System.out.println(element[0]+" "+element[1]+"\n"+element[2]+" "+element[3]);
 	}
 }
