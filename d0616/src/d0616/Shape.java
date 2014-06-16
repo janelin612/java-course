00f19dfcce48f328ed9605b87493f182fb7d3aa6
janelin612 <wgl1242254@gmail.com>
2014-06-16 19:37:55 +0800

    完成圓柱&球體 並將程式碼由Main.java移至Shape.java底下

--- d0616/src/d0616/Shape.java
+++ d0616/src/d0616/Shape.java
@@ -11,3 +11,87 @@ public abstract class Shape implements Area,Perimeter{
 	}
 	
 }
+
+class Circle extends Shape implements Area,Perimeter {
+	protected double radius;
+	public static final double pi=3.1415926;
+	
+	public Circle(double r){
+		radius=r;
+	}
+	
+	public double perimeter() {
+		return 2*radius*pi;
+	}
+
+	public double area() {
+		return radius*radius*pi;
+	}
+	
+}
+class Triangle extends Shape implements Area {
+	private double[] edge;
+	private double halfPerimeter;
+	
+	public Triangle(double edge1,double edge2,double edge3){
+		edge=new double[3];
+		edge[0]=edge1;
+		edge[1]=edge2;
+		edge[2]=edge3;
+		
+		halfPerimeter=this.perimeter()*0.5;
+	}
+	
+	public double perimeter() {
+		return edge[0]+edge[1]+edge[2];
+	}
+
+	public double area() {
+		//海龍公式
+		return Math.sqrt(halfPerimeter*(halfPerimeter-edge[0])*(halfPerimeter-edge[1])*(halfPerimeter-edge[2]));
+	}
+
+	
+}
+class Rectangle extends Shape implements Perimeter{
+	private double length;
+	private double width;
+	
+	public Rectangle(double l,double w){
+		length=l;
+		width=w;
+	}
+	
+	public double perimeter() {	
+		return 2*length*width;
+	}
+
+	public double area() {
+		return length*width;
+	}
+	
+}
+
+class Cylinder extends Circle {
+	private double hight;
+	
+	public Cylinder(double r,double h) {
+		super(r);
+		hight=h;
+	}
+	
+	public double volume(){
+		return this.area()*hight;
+	}
+}
+class Sphere extends Circle {
+
+	public Sphere(double r) {
+		super(r);
+	}
+	
+	public double volume(){
+		return 0.75*pi*radius*radius*radius;
+	}
+	
+}
