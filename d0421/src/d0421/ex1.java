7bf12e23429023532cb4ef8ae48e3e4e6e84529f
janelin612 <wgl1242254@gmail.com>
2014-06-18 21:41:21 +0800

    將防呆的部分完成

--- d0421/src/d0421/ex1.java
+++ d0421/src/d0421/ex1.java
@@ -9,15 +9,26 @@ public class ex1 {
 		Scanner scan=new Scanner(System.in);
 		
 		while(true){
-			int d = 0;
-			System.out.print("Key in a integer less than 10:");
-			//TODO 防呆
-			d=scan.nextInt();
-	
-			for(int i=0;i<d;i++){
-				System.out.print('\u0007');
+			String input;
+			System.out.print("請輸入一到三之間的數字:");
+			
+			input=scan.next();
+			if(input.equals("1")||input.equals("2")||input.equals("3")){
+				//將輸入字串轉字元陣列後取第一個元素，並配合ASCII code進行偏移
+				int times=(int)input.toCharArray()[0]-'0';
+				
+				for(int i=0;i<times;i++){
+					System.out.print('\u0007');
+				}
+				System.out.print("\n");
 			}
-			System.out.print("\n");
+			else{
+				System.out.println("範圍錯誤 請重新輸入");
+			}
+			
+				
+	
+
 		}
 	}
 }
