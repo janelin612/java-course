0beb28e21749c3c86a4626aca3dd0ae9291cca44
janelin612 <wgl1242254@gmail.com>
2014-06-22 15:00:25 +0800

    實作六速變速箱

---
+++ finalProject/src/finalProject/Transmission.java
@@ -0,0 +1,23 @@
+package finalProject;
+/*
+ * 六檔變速箱
+ */
+class Transmission {
+	GearBox[] gearBox;
+
+	public Transmission(){
+		gearBox=new GearBox[7];
+		gearBox[0]=new GearBox(0);
+		gearBox[1]=new GearBox(2.6);
+		gearBox[2]=new GearBox(1.789);
+		gearBox[3]=new GearBox(1.409);
+		gearBox[4]=new GearBox(1.160);
+		gearBox[5]=new GearBox(1.000);
+		gearBox[6]=new GearBox(0.892);
+	}
+	
+	public double getGear(int number){
+		return gearBox[number].getReductionRatio();
+	}
+	
+}
