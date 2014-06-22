71b58b522fe4721948e58635a8224fdc6104c86b
janelin612 <wgl1242254@gmail.com>
2014-06-22 15:56:57 +0800

    封裝變數 並減少過度設計

--- finalProject/src/finalProject/GearBox.java
+++ finalProject/src/finalProject/GearBox.java
@@ -5,22 +5,12 @@ package finalProject;
  */
 class GearBox {
 
-	double redcutionRatio;
-	int drivingGearTooth;
-	int passiveGearTooth;
+	private double redcutionRatio;
 	
 	public GearBox(double ratio){
 		this.redcutionRatio=ratio;
 	}
-	public GearBox(int driving,int passive){
-		this.drivingGearTooth=driving;
-		this.passiveGearTooth=passive;
-		this.redcutionRatio= (double)passive / (double)driving;
-	}
 	
-	public void setReductionRatio(double ratio){
-		this.redcutionRatio=ratio;
-	}
 	public double getReductionRatio(){
 		return this.redcutionRatio;
 	}
