0aa014462328b63e8d7f69f272e22178b6397b6f
janelin612 <wgl1242254@gmail.com>
2014-06-22 14:23:27 +0800

    完成齒輪組物件

---
+++ finalProject/src/finalProject/GearBox.java
@@ -0,0 +1,27 @@
+package finalProject;
+/*
+ * 齒輪組
+ * 
+ */
+abstract class GearBox {
+
+	double redcutionRatio;
+	int drivingGearTooth;
+	int passiveGearTooth;
+	
+	public GearBox(double ratio){
+		this.redcutionRatio=ratio;
+	}
+	public GearBox(int driving,int passive){
+		this.drivingGearTooth=driving;
+		this.passiveGearTooth=passive;
+		this.redcutionRatio= (double)passive / (double)driving;
+	}
+	
+	public void setReductionRatio(double ratio){
+		this.redcutionRatio=ratio;
+	}
+	public double getReductionRatio(){
+		return this.redcutionRatio;
+	}
+}
