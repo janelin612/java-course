ecfa5a5c5d01783ffccdc6d91eef8639100ed364
janelin612 <wgl1242254@gmail.com>
2014-06-22 17:05:18 +0800

    輪胎上註解

--- finalProject/src/finalProject/Tire.java
+++ finalProject/src/finalProject/Tire.java
@@ -3,27 +3,35 @@ package finalProject;
  * 輪胎
  */
 class Tire {
-	double width;
+	//基本常數
+	static final double pi=3.14159;
+	static final double inchToMeter=0.0254; //吋轉公尺
+	static final int secondToMinute=60; //秒轉分鐘
+	
+	double width; //輪胎寬度
 	double flatnessRatio;  //扁平比(%)
 	double insideDiameter; //內徑(吋)
 	private double ousideDiameter; //外徑(吋)
-	static final double pi=3.14159;
-	static final double inchToMeter=0.0254;
-	static final int secondToMinute=60;
+
 	
+	//constructor
 	Tire(double width,double ratio,double diameter){
 		this.width=width*0.001;
 		this.flatnessRatio=ratio*0.01;
 		this.insideDiameter=diameter;
-		calculatorOutsideDiameter();
+		calculateOutsideDiameter();
 	}
 
-	//外徑(公尺)
-	private void calculatorOutsideDiameter() {
+	//計算外徑(公尺)
+	private void calculateOutsideDiameter() {
+		/*
+		 * 外徑=內徑+2倍橡皮厚度
+		 * 橡皮厚度=扁平比*寬度
+		 */
 		this.ousideDiameter=insideDiameter*inchToMeter+2*flatnessRatio*width;
 	}
 	
-	//周長(公尺)
+	//回傳輪胎周長(公尺)
 	public double getPerimeter(){
 		return ousideDiameter*pi;
 	}
