e6fdf7be8093c2542054440882c257b21c701c0b
janelin612 <wgl1242254@gmail.com>
2014-06-22 18:43:05 +0800

    將變速箱宣告移至GearBox

--- finalProject/src/finalProject/GearBox.java
+++ finalProject/src/finalProject/GearBox.java
@@ -1,7 +1,7 @@
 package finalProject;
 /*
  * 齒輪組
- * 
+ * 以及其子類別
  */
 class GearBox {
 
@@ -32,4 +32,32 @@ class WheelAxle extends GearBox{
 		super(ratio);
 	}
 
+}
+
+//六速變速箱
+class Transmission {
+	private GearBox[] gearBox;
+	private int chosenGear;
+
+	public Transmission(){
+		chosenGear=0; //預設為空檔
+		gearBox=new GearBox[7];
+		gearBox[0]=new GearBox(0);
+		gearBox[1]=new GearBox(2.6);
+		gearBox[2]=new GearBox(1.789);
+		gearBox[3]=new GearBox(1.409);
+		gearBox[4]=new GearBox(1.160);
+		gearBox[5]=new GearBox(1.000);
+		gearBox[6]=new GearBox(0.892);
+	}
+	
+	//回傳選定檔位的減速比
+	public double getReductionRatio(){
+		return gearBox[chosenGear].getReductionRatio();
+	}
+	
+	public void setChosenGear(int i){
+		this.chosenGear=i;
+	}
+	
 }
